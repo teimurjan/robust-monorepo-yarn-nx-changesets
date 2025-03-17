47a74fd3057ce9fa027ef6a39ea5676369be8ea4
Teimur Gasanov <teymurgg321@gmail.com>
2025-03-17 12:14:05 +0600

    chore: use nx graph to check circularity

--- scripts/check-circulardeps.mjs
+++ scripts/check-circulardeps.mjs
@@ -1,23 +1,60 @@
-import { getPackages } from "@manypkg/get-packages";
-import { getDirectedDependentsGraph } from "./get-directed-dependents-graph.mjs";
+import { execSync } from "child_process";
+import path from "path";
+import fs from "fs";
 
-const checkCirculardeps = async () => {
-  const cwd = process.cwd();
-  const packages = await getPackages(cwd);
-  const dependentsGraph = getDirectedDependentsGraph(packages);
-  const cycles = dependentsGraph.getCycles();
+const hasCycle = (node, graph, visited, stack, path) => {
+  if (!visited.has(node)) {
+    visited.add(node);
+    stack.add(node);
+    path.push(node);
 
-  if (cycles.length > 0) {
-    console.error(
-      `Found circular dependencies:\n\n${cycles
-        .map((cycle) => cycle.join(" -> "))
-        .join("\n")}`
-    );
-    process.exit(1);
-  } else {
-    console.log("No circular dependencies found");
-    process.exit(0);
+    const dependencies = graph.dependencies[node] || [];
+    for (const dep of dependencies) {
+      const depNode = dep.target;
+      if (
+        !visited.has(depNode) &&
+        hasCycle(depNode, graph, visited, stack, path)
+      ) {
+        return true;
+      }
+
+      if (stack.has(depNode)) {
+        path.push(depNode);
+        return true;
+      }
+    }
   }
+
+  stack.delete(node);
+  path.pop();
+  return false;
 };
 
-checkCirculardeps();
+const getGraph = () => {
+  const cwd = process.cwd();
+  const tempOutputFilePath = path.join(cwd, "nx-graph.json");
+  execSync(`nx graph --file=${tempOutputFilePath}`, {
+    encoding: "utf-8",
+  });
+  const output = fs.readFileSync(tempOutputFilePath, "utf-8");
+  fs.rmSync(tempOutputFilePath);
+  return JSON.parse(output).graph;
+};
+
+const checkCircularDeps = () => {
+  const graph = getGraph();
+
+  const visited = new Set();
+  const stack = new Set();
+
+  for (const node of Object.keys(graph.dependencies)) {
+    const path = [];
+    if (hasCycle(node, graph, visited, stack, path)) {
+      console.error("🔴 Circular dependency detected:", path.join(" → "));
+      process.exit(1);
+    }
+  }
+  console.log("✅ No circular dependencies detected.");
+};
+
+checkCircularDeps();
